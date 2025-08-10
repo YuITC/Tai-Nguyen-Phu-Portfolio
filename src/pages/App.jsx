a7d3bf06d25a3b741695f8e1653807df1e362c17
YuITC <lehuuphuoc2502yuitc@gmail.com>
2025-08-10 12:48:15 +0700

    change 'assets' folder position

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -50,7 +50,7 @@ export default function App() {
                             </div>
 
                             <div>
-                                <a href="./assets/resumes/resume.pdf" className="inline-block px-6 py-2 rounded-md bg-gradient-to-r from-fuchsia-600 to-indigo-600 hover:from-fuchsia-500 hover:to-indigo-500 font-medium">
+                                <a href="./src/assets/resumes/resume.pdf" className="inline-block px-6 py-2 rounded-md bg-gradient-to-r from-fuchsia-600 to-indigo-600 hover:from-fuchsia-500 hover:to-indigo-500 font-medium">
                                     View my Resume
                                 </a>
                             </div>
@@ -59,7 +59,7 @@ export default function App() {
                         <div className="relative">
                             <div className="absolute inset-0 -z-10 bg-gradient-to-br from-fuchsia-600/40 via-indigo-600/30 to-transparent blur-3xl opacity-40" />
                             <div className="w-80 h-80 mx-auto rounded-3xl bg-gradient-to-br from-fuchsia-500/30 to-indigo-500/30 border border-white/10 backdrop-blur-md flex items-center justify-center text-slate-300 text-xl font-medium shadow-xl shadow-black/50">
-                                <img src="./assets/me.png" alt="Portrait of Tai Nguyen Phu" className="w-full h-full object-cover rounded-3xl" />
+                                <img src="./src/assets/me.png" alt="Portrait of Tai Nguyen Phu" className="w-full h-full object-cover rounded-3xl" />
                             </div>
                         </div>
                     </div>
